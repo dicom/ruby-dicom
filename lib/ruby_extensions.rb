3e28a73c06fe3cc9fba66eb9f202ee62722bdb84
Christoffer Lervåg <chris.lervag@gmail.com>
2009-04-11 23:20:23 +0200

    small cosmetic comment change

--- lib/ruby_extensions.rb
+++ lib/ruby_extensions.rb
@@ -1,7 +1,11 @@
+#    Copyright 2008-2009 Christoffer Lervåg
+
+# This file contains extensions to the Ruby library that is used by Ruby DICOM.
+
 class Array
   
-  # For some reason, the Ruby Array class does not have a method for
-  # finding all indices of a given value contained in an array.
+  # Searching all indices, or a subset of indices, in an array, and returning all indices
+  # where the array's value equals the queried value.
   def all_indices(array, value)
     result = []
     self.each do |pos|
@@ -10,7 +14,8 @@ class Array
     result
   end
   
-  # Similar to method above, but this one returns the position of all strings that contain the query string.
+  # Similar to method above, but this one returns the position of all strings that
+  # contain the query string (exact match not required).
   def all_indices_partial_match(array, value)
     result = []
     self.each do |pos|
