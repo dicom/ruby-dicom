3fad3902183ba320e3c226fe758a1d312eb215b8
Christoffer Lervåg <chris.lervag@gmail.com>
2009-04-25 00:30:15 +0200

    Updated syntax and comments in DWrite.rb to better reflect terminology of official DICOM standard

--- lib/DWrite.rb
+++ lib/DWrite.rb
@@ -1,20 +1,18 @@
 #    Copyright 2008-2009 Christoffer Lervåg
 
 # Some notes about this DICOM file writing class:
-# In its current state, this class will always try to write the file such that it is
-# compliant to the official standard (DICOM 3 Part 10), containing header
-# and meta information (group 0002). If this is unwanted behaviour, it is easy
-# to modify the source code here to avoid this.
+# In its current state, this class will always try to write the file such that it is compliant to the
+# official standard (DICOM 3 Part 10), containing header and meta information (group 0002).
+# If this is unwanted behaviour, it is easy to modify the source code here to avoid this.
 #
-# It is important to note, that while the goal is to be fully DICOM compliant, 
-# no guarantees are given that this is actually achieved. You are encouraged
-# to thouroughly test your files for compatibility after creation. 
-#Please contact the author if you discover any issues with file creation.
+# It is important to note, that while the goal is to be fully DICOM compliant, no guarantees are given
+# that this is actually achieved. You are encouraged to thouroughly test your files for compatibility after creation. 
+# Please contact the author if you discover any issues with file creation.
 
 module DICOM
   # Class for writing the data from DObject to a valid DICOM file:
   class DWrite
-    attr_writer :labels, :types, :lengths, :raw, :rest_endian, :rest_explicit
+    attr_writer :tags, :types, :lengths, :raw, :rest_endian, :rest_explicit
     attr_reader :success, :msg
     
     # Initialize the DWrite instance.
@@ -24,8 +22,8 @@ module DICOM
       @sys_endian = opts[:sys_endian] || false
       @file_name = file_name
       
-      # Create arrays used for storing tag information:
-      @labels = Array.new
+      # Create arrays used for storing data element information:
+      @tags = Array.new
       @types = Array.new
       @lengths = Array.new
       @raw = Array.new
@@ -41,7 +39,7 @@ module DICOM
     
     # Writes the DICOM information to file.
     def write()
-      if @labels.size > 0
+      if @tags.size > 0
         # Check if we are able to create given file:
         open_file(@file_name)
         # Read the initial header of the file:
@@ -52,11 +50,11 @@ module DICOM
           write_header()
           # Write meta information (if it is not present in the DICOM object):
           write_meta()
-          # Write tags:
-          @labels.each_index do |i|
-            write_tag(i)
+          # Write data elements:
+          @tags.each_index do |i|
+            write_data_element(i)
           end
-          # We are finished writing the tags, and as such, can close the file:
+          # We are finished writing the data elements, and as such, can close the file:
           @file.close()
           # Mark this write session as successful:
           @success = true
@@ -66,8 +64,8 @@ module DICOM
           return
         end # of if @file != nil
       else
-        @msg += ["Error. No tags to write."]
-      end # of if @labels.size > 0
+        @msg += ["Error. No data elements to write."]
+      end # of if @tags.size > 0
     end # of method write
     
     
@@ -90,16 +88,16 @@ module DICOM
       pos = Array.new()
       meta = Array.new()
       # File Meta Information Version:
-      pos += [@labels.index("0002,0001")]
+      pos += [@tags.index("0002,0001")]
       meta += [["0002,0001", "OB", 2, ["0100"].pack("H*")]]
       # Transfer Syntax UID:
-      pos += [@labels.index("0002,0010")]
+      pos += [@tags.index("0002,0010")]
       meta += [["0002,0010", "UI", 18, ["1.2.840.10008.1.2"].pack("a*")+["00"].pack("H*")]] # Implicit, little endian
       # Implementation Class UID:
-      pos += [@labels.index("0002,0012")]
+      pos += [@tags.index("0002,0012")]
       meta += [["0002,0012", "UI", 26, ["1.2.826.0.1.3680043.8.641"].pack("a*")+["00"].pack("H*")]] # Ruby DICOM UID
       # Implementation Version Name:
-      pos += [@labels.index("0002,0013")]
+      pos += [@tags.index("0002,0013")]
       meta += [["0002,0013", "SH", 10, ["RUBY_DICOM"].pack("a*")]]
       # Insert meta information:
       meta_added = false
@@ -107,14 +105,14 @@ module DICOM
         # Only insert element if it does not already exist (corresponding pos element shows no match):
         if pos[i] == nil
           meta_added = true
-          # Find where to insert this data element
+          # Find where to insert this data element.
           index = -1
-          label = meta[i][0]
+          tag = meta[i][0]
           quit = false
           while quit != true do
-            if label < @labels[index+1]
+            if tag < @tags[index+1]
               quit = true
-            elsif @labels[index+1][0..3] != "0002"
+            elsif @tags[index+1][0..3] != "0002"
               # Abort to avoid needlessly going through the whole array.
               quit = true
             else
@@ -125,13 +123,13 @@ module DICOM
           # Insert data element in the correct array position:
           if index == -1
             # Insert at the beginning of array:
-            @labels = [meta[i][0]] + @labels
+            @tags = [meta[i][0]] + @tags
             @types = [meta[i][1]] + @types
             @lengths = [meta[i][2]] + @lengths
             @raw = [meta[i][3]] + @raw
           else
             # One or more elements comes before this element:
-            @labels = @labels[0..index] + [meta[i][0]] + @labels[(index+1)..(@labels.length-1)]
+            @tags = @tags[0..index] + [meta[i][0]] + @tags[(index+1)..(@tags.length-1)]
             @types = @types[0..index] + [meta[i][1]] + @types[(index+1)..(@types.length-1)]
             @lengths = @lengths[0..index] + [meta[i][2]] + @lengths[(index+1)..(@lengths.length-1)]
             @raw = @raw[0..index] + [meta[i][3]] + @raw[(index+1)..(@raw.length-1)]
@@ -143,11 +141,11 @@ module DICOM
       quit = false
       j = 0
       while quit == false do
-        if @labels[j][0..3] != "0002"
+        if @tags[j][0..3] != "0002"
           quit = true
         else
           # Add to length if group 0002:
-          if @labels[j] != "0002,0000"
+          if @tags[j] != "0002,0000"
             if @types[j] == "OB"
               length += 12 + @lengths[j]
             else
@@ -155,22 +153,22 @@ module DICOM
             end
           end
           j += 1 
-        end # of if @labels[j][0..3]..
+        end # of if @tags[j][0..3]..
       end # of while
       # Set group length:
-      gl_pos = @labels.index("0002,0000")
+      gl_pos = @tags.index("0002,0000")
       gl_info = ["0002,0000", "UL", 4, [length].pack("I*")]
       # Update group length, but only if there have been some modifications or GL is nonexistant:
       if meta_added == true or gl_pos != nil
         if gl_pos == nil
           # Add group length (to beginning of arrays):
-          @labels = [gl_info[0]] + @labels
+          @tags = [gl_info[0]] + @tags
           @types = [gl_info[1]] + @types
           @lengths = [gl_info[2]] + @lengths
           @raw = [gl_info[3]] + @raw
         else
           # Edit existing group length:
-          @labels[gl_pos] = gl_info[0]
+          @tags[gl_pos] = gl_info[0]
           @types[gl_pos] = gl_info[1]
           @lengths[gl_pos] = gl_info[2]
           @raw[gl_pos] = gl_info[3]
@@ -179,50 +177,50 @@ module DICOM
     end # of method write_meta
     
     
-    # Writes the tags to file:
-    def write_tag(i)
-      # Step 1: Write label:
-      write_label(i)
+    # Writes each data element to file:
+    def write_data_element(i)
+      # Step 1: Write tag:
+      write_tag(i)
       # Step 2: Write [type] and value length:
       write_type_length(i)
       # Step 3: Write value:
       write_value(i)
       # If DICOM object contains encapsulated pixel data, we need some special handling for its items:
-      if @labels[i] == "7FE0,0010"
+      if @tags[i] == "7FE0,0010"
         @enc_image = true if @lengths[i].to_i == 0
       end
       # Should have some kind of test that the last data was written succesfully?
-    end # of method write_tag
+    end # of method write_data_element
     
     
-    # Writes the label (first part of the data element):
-    def write_label(i)
-      # Label is originally of the form 0002,0010.
+    # Writes the tag (first part of the data element):
+    def write_tag(i)
+      # Tag is originally of the form "0002,0010".
       # We need to reformat to get rid of the comma:
-      label = @labels[i][0..3] + @labels[i][5..8]
+      tag = @tags[i][0..3] + @tags[i][5..8]
       # Whether DICOM file is big or little endian, the first 0002 group is always little endian encoded.
       # On a big endian system, I believe the order of the numbers need not be changed,
       # but this has not been tested yet.
       if @sys_endian == false
         # System is little endian:
         # Change the order of the numbers so that it becomes correct when packed as hex:
-        label_corr = label[2..3] + label[0..1] + label[6..7] + label[4..5]
+        tag_corr = tag[2..3] + tag[0..1] + tag[6..7] + tag[4..5]
       end
       # When we shift from group 0002 to another group we need to update our endian/explicitness variables:
-      if label[0..3] != "0002" and @switched == false
+      if tag[0..3] != "0002" and @switched == false
         switch_syntax()
       end
-      # Perhaps we need to rearrange the labels if the file encoding is now big endian:
+      # Perhaps we need to rearrange the tag if the file encoding is now big endian:
       if not @endian
         # Need to rearrange the first and second part of each string:
-        label_corr = label
+        tag_corr = tag
       end
       # Write to file:
-      @file.write([label_corr].pack('H*'))
-    end # of method write_label
+      @file.write([tag_corr].pack('H*'))
+    end # of method write_tag
     
     
-    # Writes the type (if to be written) and length value (these two are the middle part of the data element):
+    # Writes the type (VR) (if it is to be written) and length value (these two are the middle part of the data element):
     def write_type_length(i)
       # First some preprocessing:
       # Set length value:
@@ -243,12 +241,12 @@ module DICOM
       # *****EXPLICIT*****:
       if @explicit == true
         # Step 1: Write VR (if it is to be written)
-        unless @labels[i] == "FFFE,E000" or @labels[i] == "FFFE,E00D" or @labels[i] == "FFFE,E0DD"
-          # Write tag type field (2 bytes - since we are not dealing with an item related tag):
+        unless @tags[i] == "FFFE,E000" or @tags[i] == "FFFE,E00D" or @tags[i] == "FFFE,E0DD"
+          # Write data element type (VR) (2 bytes - since we are not dealing with an item related element):
           @file.write([@types[i]].pack('a*'))
         end
         # Step 2: Write length
-        # Three possible structures for value length here, dependent on tag type:
+        # Three possible structures for value length here, dependent on data element type:
         case @types[i]
           when "OB","OW","SQ","UN"
             if @enc_image
@@ -264,11 +262,11 @@ module DICOM
             end
           when "()"
             # 4 bytes:
-            # For labels "FFFE,E000", "FFFE,E00D" and "FFFE,E0DD"
+            # For tags "FFFE,E000", "FFFE,E00D" and "FFFE,E0DD"
             @file.write(length4)
           else
             # 2 bytes:
-            # For all the other tag types, value length is 2 bytes:
+            # For all the other data element types, value length is 2 bytes:
             @file.write(length2)
         end # of case type
       else
@@ -324,7 +322,7 @@ module DICOM
 
     # Changes encoding variables as the file writing proceeds past the initial 0002 group of the DICOM file.
     def switch_syntax()
-      # The information from the Transfer syntax tag (if present), needs to be processed:
+      # The information from the Transfer syntax element (if present), needs to be processed:
       process_transfer_syntax()
       # We only plan to run this method once:
       @switched = true
@@ -340,9 +338,9 @@ module DICOM
     end
 
 
-    # Checks the Transfer Syntax UID tag and updates class variables to prepare for correct writing of DICOM file.
+    # Checks the Transfer Syntax UID element and updates class variables to prepare for correct writing of DICOM file.
     def process_transfer_syntax()
-      ts_pos = @labels.index("0002,0010")
+      ts_pos = @tags.index("0002,0010")
       if ts_pos != nil
         ts_value = @raw[ts_pos].unpack('a*').to_s.rstrip
         valid = @lib.check_ts_validity(ts_value)
@@ -424,7 +422,7 @@ module DICOM
       end
       # Set which format strings to use when unpacking numbers:
       set_pack_strings
-      # Items contained under the pixel data tag needs some special attention to write correctly:
+      # Items contained under the Pixel Data element needs some special attention to write correctly:
       @enc_image = false
     end # of method init_variables
   
