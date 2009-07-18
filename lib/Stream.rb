051a0c8bd20e7d87313ef4f769ce2d1ec61f4d94
cuthbert <chris.lervag@gmail.com>
2009-07-18 21:33:24 +0200

    Updated the Stream class to also handle Write operations

--- lib/Stream.rb
+++ lib/Stream.rb
@@ -7,44 +7,42 @@
 # gathered in this one class in an attemt to minimize code duplication.
 
 module DICOM
-  # Class for handling string operations:
+  # Class for handling binary string operations:
   class Stream
 
-    attr_accessor :endian, :explicit, :index
+    attr_accessor :endian, :explicit, :index, :string
     attr_reader :errors
 
+    # Initialize the Stream instance.
     def initialize(string, str_endian, explicit, options={})
       # Set instance variables:
       @explicit = explicit # true or false
-      @string = string # input binary string, or nil if empty
+      @string = string # input binary string
+      @string = "" unless @string # if nil, change it to an empty string
       @index = options[:index] || 0
+      @errors = Array.new
       set_endian(str_endian) # true or false
     end
 
-    # Determine the endianness of the system.
-    # Together with the specified endianness of the binary string,
-    # this will decide what encoding/decoding flags to use.
-    def configure_endian
-      x = 0xdeadbeef
-      endian_type = {
-        Array(x).pack("V*") => false, #:little
-        Array(x).pack("N*") => true   #:big
-      }
-      @sys_endian = endian_type[Array(x).pack("L*")]
-      # Use a "relationship endian" variable to guide encoding/decoding options:
-      if @sys_endian == @str_endian
-        @endian = true
-      else
-        @endian = false
-      end
+
+    # Adds a pre-encoded string to the end of this instance's string.
+    def add_first(binary)
+      @string = binary + @string if binary
     end
+
+
+    # Adds a pre-encoded string to the beginning of this instance's string.
+    def add_last(binary)
+      @string = @string + binary if binary
+    end
+
 
     # Decodes a section of the binary string and returns the formatted data.
     def decode(length, type, options = {})
       # Check if values are valid:
       if (@index + length) > @string.length
-        # The index number is bigger then the length of the binary string
-        # we have reached the end and will return nil.
+        # The index number is bigger then the length of the binary string.
+        # We have reached the end and will return nil.
         value = nil
       else
         # Decode the string, unless the binary string itself is wanted:
@@ -64,6 +62,114 @@ module DICOM
       return value
     end
 
+
+    # Decodes a tag from a binary string to our standard ascii format ("GGGG,EEEE").
+    def decode_tag
+      length = 4
+      # Check if values are valid:
+      if (@index + length) > @string.length
+        # The index number is bigger then the length of the binary string.
+        # We have reached the end and will return nil.
+        tag = nil
+      else
+        # Decode and process:
+        string = @string.slice(@index, length).unpack(@hex)[0].upcase
+        if @endian
+          tag = string[2..3] + string[0..1] + "," + string[6..7] + string[4..5]
+        else
+          tag = string[0..3] + "," + string[4..7]
+        end
+        # Update our position in the string:
+        skip(length)
+      end
+      return tag
+    end
+
+
+    # Encodes content (string, number, array of numbers) and returns the binary string.
+    def encode(value, type)
+      bin = [value].pack(vr_to_str(type))
+    end
+
+
+    # Encodes content (string, number, array of numbers) to a binary string and pastes it to
+    # the beginning of the @string variable of this instance.
+    def encode_first(value, type)
+      bin = [value].pack(vr_to_str(type))
+      @string = bin + @string
+    end
+
+
+    # Encodes content (string, number, array of numbers) to a binary string and pastes it to
+    # the end of the @string variable of this instance.
+    def encode_last(value, type)
+      bin = [value].pack(vr_to_str(type))
+      @string = @string + bin
+    end
+
+
+    # Some of the strings that are passed along in the network communication are allocated a fixed length.
+    # This method is used to encode such strings.
+    def encode_string_with_trailing_spaces(string, target_length)
+      length = string.length
+      if length < target_length
+        return [string].pack(@str)+["20"*(target_length-length)].pack(@hex)
+      elsif length == target_length
+        return [string].pack(@str)
+      else
+        raise "The string provided is longer than the allowed maximum length. (string: #{string}, target_length: #{target_length.to_s})"
+      end
+    end
+
+
+    # Encodes a tag from its standard text format ("GGGG,EEEE"), to a proper binary string.
+    def encode_tag(string)
+      if @endian
+        tag = string[2..3] + string[0..1] + string[7..8] + string[5..6]
+      else
+        tag = string[0..3] + string[5..8]
+      end
+      return [tag].pack(@hex)
+    end
+
+
+    # Encodes and returns a data element value. The reason for not using the encode()
+    # method for this is that we may need to know the length of the encoded value before
+    # we send it to the encode() method.
+    # String values will be checked for possible odd length, and if so padded with an extra byte,
+    # to comply with the DICOM standard.
+    def encode_value(value, type)
+      type = vr_to_str(type)
+      if type == @str
+        # String: Check length.
+        if value.length[0] == 1
+          # Odd length (add a zero byte, encode and return):
+          return [value].pack(type)+["00"].pack(@hex)
+        else
+          # Even length (encode and return):
+          return [value].pack(type)
+        end
+      elsif type == @hex
+        return [value].pack(type)
+      else
+        # Number.
+        return [value].pack(type)
+      end
+    end
+
+
+    # Reset the string variable.
+    def reset
+      @string = ""
+    end
+
+
+    # Reset the string index variable:
+    def reset_index
+      @index = 0
+    end
+
+
     # This method updates the endianness to be used for the binary string, and checks
     # the system endianness to determine which encoding/decoding flags to use.
     def set_endian(str_endian)
@@ -74,6 +180,7 @@ module DICOM
       set_format_hash
     end
 
+
     # Set the hash which is used to convert a data element type (VR) to a encode/decode string.
     def set_format_hash
       @format = {
@@ -81,10 +188,10 @@ module DICOM
         "SL" => @sl, # Signed long (4 bytes)
         "US" => @us, # Unsigned short (2 bytes)
         "SS" => @ss, # Signed short (2 bytes)
-        "FL" => @fl, # Floating point single (4 bytes)
+        "FL" => @fs, # Floating point single (4 bytes)
         "FD" => @fd, # Floating point double (8 bytes)
         "OB" => @by, # Other byte string (1-byte integers)
-        "OF" => @fl, # Other float string (4-byte floating point numbers)
+        "OF" => @fs, # Other float string (4-byte floating point numbers)
         "OW" => @us, # Other word string (2-byte integers)
         "AT" => @hex, # Tag reference (4 bytes) NB: This may need to be revisited at some point...
         "UN" => @hex, # Unknown information (header element is not recognized from local database)
@@ -108,6 +215,7 @@ module DICOM
         "STR" => @str
       }
     end
+
 
     # Sets the pack/unpack format strings that will be used for encoding/decoding.
     # Some of these will depend on the endianness of the system and file.
@@ -135,13 +243,38 @@ module DICOM
       end
       # Format strings that are not dependent on endianness:
       @str = "a*"
-      @hex = "H*" # (this may be dependent on endianness, not tested yet..)
+      @hex = "H*" # (this may be dependent on endianness(?))
     end
+
 
     # Applies an offset (positive or negative) to the index variable.
     def skip(offset)
       @index += offset
     end
+
+
+    # Following methods are private:
+    private
+
+
+    # Determine the endianness of the system.
+    # Together with the specified endianness of the binary string,
+    # this will decide what encoding/decoding flags to use.
+    def configure_endian
+      x = 0xdeadbeef
+      endian_type = {
+        Array(x).pack("V*") => false, #:little
+        Array(x).pack("N*") => true   #:big
+      }
+      @sys_endian = endian_type[Array(x).pack("L*")]
+      # Use a "relationship endian" variable to guide encoding/decoding options:
+      if @sys_endian == @str_endian
+        @endian = true
+      else
+        @endian = false
+      end
+    end
+
 
     # Convert a data element type (VR) to a encode/decode string.
     def vr_to_str(vr)
@@ -153,5 +286,5 @@ module DICOM
       return str
     end
 
-  end
-end
+  end # of class
+end # of module
