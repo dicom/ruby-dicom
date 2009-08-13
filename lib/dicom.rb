283ceee095c4cc1b942f248369a021fb0105dfc7
cuthbert <chris.lervag@gmail.com>
2009-08-13 21:26:01 +0200

    Updated the library load file with the 3 new files that are added to Ruby DICOM

--- lib/dicom.rb
+++ lib/dicom.rb
@@ -1,9 +1,12 @@
 # Core library:
 require 'Dictionary'
+require 'DClient'
 require 'DLibrary'
 require 'DObject'
 require 'DRead'
+require 'DServer'
 require 'DWrite'
+require 'Link'
 require 'Stream'
 # Extended library:
 require 'Anonymizer'
