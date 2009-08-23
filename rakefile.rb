50936272cba6a044d2a62fd84db291289f738f00
cuthbert <chris.lervag@gmail.com>
2009-08-23 22:47:23 +0200

    Updated CHANGELOG and rakefile. This is released as version 0.6.1

--- rakefile.rb
+++ rakefile.rb
@@ -10,7 +10,7 @@ require 'rake/gempackagetask'
 spec = Gem::Specification.new do |s|
   s.platform = Gem::Platform::RUBY
   s.name = "dicom"
-  s.version = "0.6"
+  s.version = "0.6.1"
   s.date = Time.now
   s.summary = "Library for handling DICOM files and DICOM network communication."
   s.require_paths = ["lib"]
