9ce66dc80935816ead91975aec429d014e9f3527
cuthbert <chris.lervag@gmail.com>
2009-12-12 22:49:13 +0100

    Update dicom.rb to load files from new folder

--- lib/dicom.rb
+++ lib/dicom.rb
@@ -1,17 +1,17 @@
 # Core library:
-require 'DClient'
-require 'Dictionary'
-require 'DLibrary'
-require 'DObject'
-require 'DRead'
-require 'DServer'
-require 'DWrite'
-require 'Link'
-require 'Stream'
+require 'dicom/DClient'
+require 'dicom/Dictionary'
+require 'dicom/DLibrary'
+require 'dicom/DObject'
+require 'dicom/DRead'
+require 'dicom/DServer'
+require 'dicom/DWrite'
+require 'dicom/Link'
+require 'dicom/Stream'
 # Extended library:
-require 'Anonymizer'
+require 'dicom/Anonymizer'
 # Ruby library extensions:
-require 'ruby_extensions'
+require 'dicom/ruby_extensions'
 
 # Ruby DICOM version string:
 DICOM::VERSION = "0.6.1"
