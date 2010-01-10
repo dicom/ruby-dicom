e38ea46de7b4b91f1a8572309791e4aa6d7446e2
cuthbert <chris.lervag@gmail.com>
2010-01-10 17:42:37 +0100

    Implemented a proper test for tag validity in method is_a_tag? by using regex matching on the string group and element content

--- lib/dicom/ruby_extensions.rb
+++ lib/dicom/ruby_extensions.rb
@@ -9,7 +9,7 @@ class Array
     self.each do |pos|
       result << pos if array[pos] == value
     end
-    result
+    return result
   end
   
   # Similar to method above, but this one returns the position of all strings that
@@ -19,18 +19,24 @@ class Array
     self.each do |pos|
       result << pos if array[pos].include?(value)
     end
-    result
+    return result
   end
 
 end
 
 class String
   
-  # Check if a given string appears to be a valid tag:
+  # Check if a given string appears to be a valid tag (GGGG,EEEE).
+  # Three tests are performed on our string: Length (9), presence of comma in position 5,
+  # and that the rest of the characters are valid DICOM tag characters.
+  # (It might be that this can be simplified to one single test with a more advanced regex formula)
   def is_a_tag?
     result = false
-    result = true if self.length == 9 and self[4..4] == ','
-    result
+    if self.length == 9 and self[4..4] == ','
+      clean = self.delete(',').downcase
+      result = true unless clean =~ /([^a-f0-9])/
+    end
+    return result
   end
   
 end
