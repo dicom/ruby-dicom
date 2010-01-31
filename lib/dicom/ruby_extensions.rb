bbd13b491a1d34d3e04d3b087485edabbae326d0
cuthbert <chris.lervag@gmail.com>
2010-01-31 17:04:13 +0100

    Fixed an incompatibility with Ruby 1.8

--- lib/dicom/ruby_extensions.rb
+++ lib/dicom/ruby_extensions.rb
@@ -31,14 +31,16 @@ class String
 	# a comma, then 4 new HEX characters, which constitutes the tag format used by Ruby DICOM.
   def is_a_tag?
     result = false
-		result = true if self =~ /\A\h{4},\h{4}\z/
+		#result = true if self =~ /\A\h{4},\h{4}\z/ # (turns out the hex reference '\h' isnt compatible with ruby 1.8)
+    result = true if self =~ /\A[a-fA-F\d]{4},[a-fA-F\d]{4}\z/
     return result
   end
 
   # Check if a given tag string indicates a private tag (Odd group number) by doing a regexp matching.
   def private?
     result = false
-    result = true if self.upcase =~ /\A\h{3}[1,3,5,7,9,B,D,F],\h{4}\z/
+    #result = true if self.upcase =~ /\A\h{3}[1,3,5,7,9,B,D,F],\h{4}\z/ # (incompatible with ruby 1.8)
+    result = true if self.upcase =~ /\A[a-fA-F\d]{3}[1,3,5,7,9,B,D,F],[a-fA-F\d]{4}\z/
     return result
   end
 
