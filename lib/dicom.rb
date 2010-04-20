df581f37586d34b1936537378f610dfe87bf471e
cuthbert <chris.lervag@gmail.com>
2010-04-20 22:46:55 +0200

    **This marks the first commit of the major Data Element rewrite** Some updates to the dicom.rb gem load file.

--- lib/dicom.rb
+++ lib/dicom.rb
@@ -1,4 +1,10 @@
 # Core library:
+# Super classes/modules (which needs to be loaded in a proper sequence):
+require 'dicom/SuperParent'
+require 'dicom/SuperItem'
+require 'dicom/Elements'
+# Subclasses and independent classes:
+require 'dicom/DataElement'
 require 'dicom/DClient'
 require 'dicom/Dictionary'
 require 'dicom/DLibrary'
@@ -7,19 +13,36 @@ require 'dicom/DRead'
 require 'dicom/DServer'
 require 'dicom/DWrite'
 require 'dicom/FileHandler'
+require 'dicom/Item'
 require 'dicom/Link'
+require 'dicom/Sequence'
 require 'dicom/Stream'
-# Extended library:
-require 'dicom/Anonymizer'
 # Extensions to the Ruby library:
 require 'dicom/ruby_extensions'
 
-# Ruby DICOM version string:
-DICOM::VERSION = "0.7.1b"
+# Extended library:
+require 'dicom/Anonymizer'
 
-# Load the DICOM Library class (dictionary):
-DICOM::LIBRARY =  DICOM::DLibrary.new
+
+# Ruby DICOM version string:
+DICOM::VERSION = "0.7.5b"
 
 # Ruby DICOM implementation name and uid:
 DICOM::NAME = "RUBY_DICOM_" + DICOM::VERSION
 DICOM::UID = "1.2.826.0.1.3680043.8.641"
+
+# Item tag:
+DICOM::ITEM_TAG = "FFFE,E000"
+# Item related tags (includes both types of delimitation items):
+DICOM::ITEM_TAGS = ["FFFE,E000", "FFFE,E00D", "FFFE,E0DD"]
+# Delimiter tags (includes both types of delimitation items):
+DICOM::DELIMITER_TAGS = ["FFFE,E00D", "FFFE,E0DD"]
+
+# VR used for the item elements:
+DICOM::ITEM_VR = "()"
+
+# Pixel tag:
+DICOM::PIXEL_TAG = "7FE0,0010"
+
+# Load the DICOM Library class (dictionary):
+DICOM::LIBRARY =  DICOM::DLibrary.new
