f6da3d138822940e569287a8c0a5747d9e559ba0
Björn Albers <bjoernalbers@googlemail.com>
2010-11-15 15:11:29 +0100

    rspec setup with mocha framework

--- rakefile.rb
+++ rakefile.rb
@@ -4,6 +4,7 @@
 require 'rubygems'
 require 'rake/gempackagetask'
 require 'rake/testtask'
+require 'rspec/core/rake_task'
 
 spec = eval(File.read('dicom.gemspec'))
 Rake::GemPackageTask.new(spec) do |pkg|
@@ -23,4 +24,10 @@ namespace :test do
     t.verbose = true
   end
 
+end
+
+# RSpec 2.x
+RSpec::Core::RakeTask.new do |t|
+  t.rspec_opts = ["-c", "-f progress", "-r ./spec/spec_helper.rb"]
+  t.pattern = 'spec/**/*_spec.rb'
 end
