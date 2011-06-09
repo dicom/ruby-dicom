59b5f3e143359e33ec7e26fe6ad341cddcbf53e7
icdark <icdark@gmail.com>
2011-06-09 19:01:11 +0200

    extended Logging description, added DICOM as progname to Logging format

--- lib/dicom/logging.rb
+++ lib/dicom/logging.rb
@@ -2,58 +2,84 @@ module DICOM
 
   # This module handles logging functionality
   #
+  # Logging functionality uses standard library's Logger.
+  #
+  # === Examples
+  #
+  #   require 'dicom'
+  #   include DICOM
+  #   require 'logger'
+  #
+  #   # logging to STDOUT with DEBUG level
+  #   Logging.logger = Logger.new(STDOUT)
+  #   Logging.level = Logger::DEBUG
+  #
+  #   # logging to file
+  #   Logging.logger = Logger.new('my_logfile.log')
+  #
+  #   # Examples from Logger doc:
+  #
+  #   # Create a logger which ages logfile once it reaches a certain
+  #   # size. Leave 10 "old log files" and each file is about 1,024,000
+  #   # bytes.
+  #   #
+  #   Logging.logger = Logger.new('foo.log', 10, 1024000)
+  #
+  #   #  Create a logger which ages logfile daily/weekly/monthly.
+  #   #
+  #   Logging.logger = Logger.new('foo.log', 'daily')
+  #   Logging.logger = Logger.new('foo.log', 'weekly')
+  #   Logging.logger = Logger.new('foo.log', 'monthly')
+  #
+  #
+  #   For more information please read the Logger documentation.
+  #
   module Logging
     require "logger"
 
-    # Logging functionality uses standard library's Logger.
+    # logger object setter
     #
-    # === Examples
+    # === Example
     #
-    #   require 'dicom'
-    #   include DICOM
-    #   require 'logger'
+    # Logging.logger = Logger.new(STDOUT)
     #
-    #   # logging to STDOUT with DEBUG level
-    #   Logging.logger = Logger.new(STDOUT)
-    #   Logging.level = Logger::DEBUG
-    #
-    #   # logging to file
-    #   Logging.logger = Logger.new('my_logfile.log')
-    #
-    #   # Examples from Logger doc:
-    #
-    #   # Create a logger which ages logfile once it reaches a certain
-    #   # size. Leave 10 "old log files" and each file is about 1,024,000
-    #   # bytes.
-    #   # 
-    #   Logging.logger = Logger.new('foo.log', 10, 1024000)
-    #
-    #   #  Create a logger which ages logfile daily/weekly/monthly.
-    #   # 
-    #   Logging.logger = Logger.new('foo.log', 'daily')
-    #   Logging.logger = Logger.new('foo.log', 'weekly')
-    #   Logging.logger = Logger.new('foo.log', 'monthly')
-    #   
-    #
-    #   For more information please read the Logger documentation.
     def self.logger=(obj)
       @logger = obj
     end
 
+    # logger object getter
+    #
+    # === Example
+    #
+    # Logging.logger.class # => Logger
+    #
     def self.logger
-      @logger ||= if defined?(Rails) 
-                    Rails.logger 
+      @logger ||= if defined?(Rails)
+                    Rails.logger
                   else
                     logger = Logger.new(STDOUT)
                     logger.level = Logger::INFO
+                    logger.progname = "DICOM"
                     logger
                   end
     end
 
+    # logging level getter
+    #
+    # === Example
+    #
+    # Logging.level # => Logger::INFO
+    #
     def self.level
       @logger.level
     end
 
+    # logging level setter
+    #
+    # === Example
+    #
+    # Logging.level = Logger::INFO
+    #
     def self.level=(obj)
       @logger.level = obj
     end
