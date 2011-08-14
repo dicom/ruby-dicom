e3e6a38bde071db8f74e96fea29e9051e642a518
Christoffer Lervåg <chris.lervag@gmail.com>
2011-08-14 14:15:54 +0200

    Improved the documentation of the Logging module. Removed an apparently unused setter method. Simplified README logging information.

--- lib/dicom/logging.rb
+++ lib/dicom/logging.rb
@@ -1,73 +1,86 @@
 module DICOM
 
-  # This module handles logging functionality
+  # This module handles logging functionality.
   #
-  # Logging functionality uses standard library's Logger.
-  # To prevent change on progname which inside of DICOM module is simply "DICOM"
-  # we use proxy class.
-  #
+  # Logging functionality uses the Standard library's Logger class.
+  # To properly handle progname, which inside the DICOM module is simply
+  # "DICOM", in all cases, we use an implementation with a proxy class.
   #
   # === Examples
   #
   #   require 'dicom'
   #   include DICOM
   #
-  #   # logging to STDOUT with DEBUG level
+  #   # Logging to STDOUT with DEBUG level:
   #   DICOM.logger = Logger.new(STDOUT)
   #   DICOM.logger.level = Logger::DEBUG
   #
-  #   # logging to file
+  #   # Logging to a file:
   #   DICOM.logger = Logger.new('my_logfile.log')
   #
-  #   # Examples from Logger doc:
-  #
-  #   # Create a logger which ages logfile once it reaches a certain
-  #   # size. Leave 10 "old log files" and each file is about 1,024,000
-  #   # bytes.
-  #   #
-  #   DICOM.logger = Logger.new('foo.log', 10, 1024000)
-  #
-  #   #  Create a logger which ages logfile daily/weekly/monthly.
-  #   #
-  #   DICOM.logger = Logger.new('foo.log', 'daily')
-  #   DICOM.logger = Logger.new('foo.log', 'weekly')
-  #   DICOM.logger = Logger.new('foo.log', 'monthly')
-  #
-  #
-  #   # Combine external logger with DICOM
-  #   #
+  #   # Combine an external logger with DICOM:
   #   logger = Logger.new(STDOUT)
   #   logger.progname = "MY_APP"
   #   DICOM.logger = logger
-  #   # now you can call
-  #   DICOM.logger.info "Message"            # => "DICOM: Message"
-  #   DICOM.logger.info("MY_MODULE)"Message" # => "MY_MODULE: Message"
-  #   logger.info "Message"                  # => "MY_APP: Message"
+  #   # Now you can call the logger in the following ways:
+  #   DICOM.logger.info "Message"               # => "DICOM: Message"
+  #   DICOM.logger.info("MY_MODULE) {"Message"} # => "MY_MODULE: Message"
+  #   logger.info "Message"                     # => "MY_APP: Message"
   #
-  #   For more information please read the Logger documentation.
+  #   For more information, please read the Standard library Logger documentation.
   #
-
   module Logging
-    require "logger"
+    
+    require 'logger'
 
+    # Inclusion hook to make the ClassMethods available to whatever
+    # includes the Logging module, i.e. the DICOM module.
+    #
     def self.included(base)
       base.extend(ClassMethods)
     end
 
-
     module ClassMethods
 
+      # We use our own ProxyLogger to achieve the features wanted for DICOM logging,
+      # e.g. using DICOM as progname for messages logged within the DICOM module
+      # (for both the Standard logger as well as the Rails logger), while still allowing
+      # a custom progname to be used when the logger is called outside the DICOM module.
+      #
       class ProxyLogger
-        def initialize(target_object)
-          @target = target_object
+        
+        # Creating the ProxyLogger instance.
+        #
+        # === Parameters
+        #
+        # * <tt>target</tt> -- A Logger instance (e.g. Standard Logger or ActiveSupport::BufferedLogger).
+        #
+        def initialize(target)
+          @target = target
         end
 
+        # Catches missing methods.
+        # In our case, the methods of interest are the typical logger methods,
+        # i.e. log, info, fatal, error, debug, where the arguments/block are
+        # redirected to the logger in a specific way so that our stated logger
+        # features are achieved (this behaviour depends on the logger
+        # (Rails vs Standard) and in the case of Standard logger,
+        # whether or not a block is given).
+        #
+        # === Examples
+        #
+        #   # Inside the DICOM module or an external class with 'include DICOM::Logging':
+        #   logger.info "message"
+        #
+        #   # Calling from outside the DICOM module:
+        #   DICOM.logger.info "message"
+        #
         def method_missing(method_name, *args, &block)
           if method_name.to_s =~ /(log|info|fatal|error|debug)/
-            # rails use their own buffered logger which does not
-            # works with progname + block as the std logger does
+            # Rails uses it's own buffered logger which does not
+            # work with progname + block as the standard logger does:
             if defined?(Rails)
-              @target.send(method_name, "DICOM: " + args.first)
+              @target.send(method_name, "DICOM: #{args.first}")
             elsif block_given?
               @target.send(method_name, *args) { yield }
             else
@@ -77,59 +90,69 @@ module DICOM
             @target.send(method_name, *args, &block)
           end
         end
+        
       end
 
-      # logger class instance (covered by proxy)
+      # The logger class variable (must be initialized
+      # before it is referenced by the object setter).
       #
       @@logger = nil
 
-      # logger object setter
+      # The logger object setter.
+      # This method is used to replace the default logger instance with
+      # a custom logger of your own. 
       #
-      # === Example
+      # === Parameters
       #
-      # # inside a class with "include Logging"
-      # logger.info "message"
+      # * <tt>l</tt> -- A Logger instance (e.g. a custom standard Logger).
       #
-      # # outside
-      # DICOM.logger.info "message"
+      # === Examples
+      #
+      #   # Create a logger which ages logfile once it reaches a certain size,
+      #   # leaves 10 "old log files" with each file being about 1,024,000 bytes:
+      #   DICOM.logger = Logger.new('foo.log', 10, 1024000)
       #
       def logger=(l)
         @@logger = ProxyLogger.new(l)
       end
 
-      # logger object getter
+      # The logger object getter.
+      # Returns the logger class variable, if defined.
+      # If not defined, sets up the Rails logger (if in a Rails environment),
+      # or a Standard logger if not.
       #
-      # === Example
+      # === Examples
       #
-      # # inside a class with "include Logging"
-      # logger # => Logger instance
+      #   # Inside the DICOM module (or a class with 'include DICOM::Logging'):
+      #   logger # => Logger instance
       #
-      # # outside
-      # DICOM.logger # => Logger instance
+      #   # Accessing from outside the DICOM module:
+      #   DICOM.logger # => Logger instance
       #
       def logger
         @@logger ||= lambda {
           if defined?(Rails)
-            ProxyLogger.new Rails.logger
+            ProxyLogger.new(Rails.logger)
           else
             l = Logger.new(STDOUT)
             l.level = Logger::INFO
-            ProxyLogger.new l
+            ProxyLogger.new(l)
           end
         }.call
       end
+      
     end
 
+    # A logger object getter.
+    # Forwards the call to the logger class method of the Logging module.
+    #
     def logger
       self.class.logger
     end
 
-    def logger=(l)
-      self.class.logger = l
-    end
-
   end
 
-  # so we can use DICOM.logger
+  # Include the Logging module so we can use DICOM.logger.
   include Logging
+  
 end
