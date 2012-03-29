36e79400443267492c5716db78ba5fb7e3888d02
Christoffer Lervåg <chris.lervag@gmail.com>
2012-03-29 18:32:54 +0200

    Minor documentation update
    Specified that the UID constant is the DICOM UID root of ruby-dicom.

--- lib/dicom/constants.rb
+++ lib/dicom/constants.rb
@@ -1,7 +1,6 @@
-
 module DICOM
 
-  # Ruby DICOM's Implementation UID.
+  # Ruby DICOM's registered DICOM UID root (Implementation Class UID).
   UID = "1.2.826.0.1.3680043.8.641"
   # Ruby DICOM name & version (max 16 characters).
   NAME = "RUBY_DCM_" + DICOM::VERSION
