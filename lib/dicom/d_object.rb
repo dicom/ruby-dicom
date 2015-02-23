b3ee0c5f85b59e35792e029e3db9569533cc65e7
Christoffer Lervåg <chris.lervag@gmail.com>
2015-02-23 09:31:49 +0100

    Update year

--- lib/dicom/d_object.rb
+++ lib/dicom/d_object.rb
@@ -1,4 +1,4 @@
-#    Copyright 2008-2014 Christoffer Lervag
+#    Copyright 2008-2015 Christoffer Lervag
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
